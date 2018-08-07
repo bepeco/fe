09f6b093dc1c2a81691bdef78c7f80618e7997e6
mysticPrg <mysticPrg@gmail.com>
2018-08-07 14:44:02 +0900

    standard 적용 (lint 명령어 추가 및 webpack 설정)

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const Webpack = require("webpack");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const Webpack = require('webpack')
 
-const distPath = "dist";
+const distPath = 'dist'
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    chunkFilename: "[name].bundle.js",
+    filename: 'bundle.js',
+    chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, distPath)
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   devServer: {
     contentBase: path.resolve(__dirname, distPath),
     port: 9000,
@@ -21,20 +21,29 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin([distPath]),
     new HtmlWebpackPlugin({
-      template: "public/index.html"
+      template: 'public/index.html'
     }),
     new Webpack.HotModuleReplacementPlugin()
   ],
   module: {
     rules: [
       {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'standard-loader',
+        exclude: /node_modules/,
+        options: {
+          parser: 'babel-eslint'
+        }
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: ['babel-loader']
       }
     ]
   },
   resolve: {
-    extensions: ["*", ".js"]
+    extensions: ['*', '.js']
   }
-};
+}
